3efbfabebbf14224a398b8df9eb98723c460b1eb
Jordan Harband <ljharb@gmail.com>
2017-11-12 08:18:05 -0800

    [Dev Deps] update `eslint`, `eslint-config-airbnb`, `eslint-plugin-import`, `eslint-plugin-jsx-a11y`, `mocha`, `sinon`

--- test/components/HypernovaExampleReact.js
+++ test/components/HypernovaExampleReact.js
@@ -1,4 +1,4 @@
-const renderReact = require('../../').renderReact;
+const { renderReact } = require('../../');
 const Component = require('./ExampleReactComponent.js');
 
 module.exports = renderReact('HypernovaExampleReact.js', Component);
