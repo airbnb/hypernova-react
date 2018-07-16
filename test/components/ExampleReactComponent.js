7486538c57843cec2895f17d3e4ac06ce5eec882
Jordan Harband <ljharb@gmail.com>
2018-07-15 23:16:22 -0700

    [Dev Deps] update `babel-preset-airbnb`, `eslint-config-airbnb`, `eslint-plugin-import`, `eslint-plugin-jsx-a11y`, `eslint-plugin-react`, `hypernova`, `prop-types`, `sinon`, `sinon-sandbox`

--- test/components/ExampleReactComponent.js
+++ test/components/ExampleReactComponent.js
@@ -1,9 +1,9 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-function ExampleReactComponent(props) {
-  const name = ['Hello', props.name];
-  return React.createElement('div', { id: 'example' }, name.join(' '));
+function ExampleReactComponent({ name }) {
+  const hi = ['Hello', name];
+  return React.createElement('div', { id: 'example' }, hi.join(' '));
 }
 
 ExampleReactComponent.propTypes = {
